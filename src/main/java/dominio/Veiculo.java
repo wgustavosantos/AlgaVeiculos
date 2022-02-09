dcd0bcc09cf573a66176cfeefb6240ffe5b9ab81
Wenderson Santos <guto15santos@gmail.com>
2022-02-09 08:54:38 -0300

    Mapeamento basico e persistence.xml

--- src/main/java/dominio/Veiculo.java
+++ src/main/java/dominio/Veiculo.java
@@ -1,7 +1,17 @@
 package dominio;
 import java.math.BigDecimal;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
 public class Veiculo {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long codigo;
 	private String fabricante;
 	private String modelo;
